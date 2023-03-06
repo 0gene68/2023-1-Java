fa2d8bd46bc55aa50602ba8776ced90e00133e33
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-03-06 22:09:48 +0900

    1st committing(Update comment)
    It's my 1st committing about OOP, to print the information about Television Object.

--- Box.java
+++ Box.java
@@ -3,11 +3,6 @@ public class Box {
 	int length; //상자의 세로 길이
 	int height; //상자의 높이
 
-	//객체의 정보를 출력할 print() 메소드 생성
-	void print(){
-		System.out.println("상자의 가로, 세로, 높이는 " + width + ", " + length + ", " + height + "입니다. ");
-	}
-
 	public static void main(String[] args) {
 		//박스 객체 생성
 		Box b = new Box();
@@ -16,7 +11,8 @@ public class Box {
 		b.length = 20; //상자의 세로 길이를 20으로 설정
 		b.height = 30; //상자의 높이를 30으로 설정
 		
-		b.print(); //박스 객체 정보 출력
+		//객체 정보 상태 출력
+		System.out.println("상자의 가로, 세로, 높이는 " + width + ", " + length + ", " + height + "입니다. ");
 		
 	}
 }
