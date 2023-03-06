6f32b76aa0563a4df3e41bdb26287847477ed668
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-03-06 20:17:23 +0900

    Update comment

--- Box.java
+++ Box.java
@@ -9,14 +9,14 @@ public class Box {
 	}
 
 	public static void main(String[] args) {
-		//객체 생성
+		//박스 객체 생성
 		Box b = new Box();
 		
 		b.width = 20; //상자의 가로 길이를 20으로 설정
 		b.length = 20; //상자의 세로 길이를 20으로 설정
 		b.height = 30; //상자의 높이를 30으로 설정
 		
-		b.print(); //객체 정보 출력
+		b.print(); //박스 객체 정보 출력
 		
 	}
 }
