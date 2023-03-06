f9e0170571ecd97d11cb4c391e6a6b39f9e8f650
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-03-06 22:22:09 +0900

    2nd committing(Update comments and add new method)

--- Television.java
+++ Television.java
@@ -1,22 +1,27 @@
 
 public class Television {
-	int channel; //텔레비전의 채널
-	int volume; //텔레비전의 볼륨
-	boolean isOn; //텔레비전의 전원 on/off 상태(true -> on / false -> off)
+	int channel; //채널
+	int volume; //볼륨
+	boolean isOn; //전원 on/off 상태(true -> on / false -> off)
 
-	//텔레비전 객체에 대한 정보 출력
+	//객체에 대한 정보 출력
 	void print() {
-		System.out.println("채널은" + channel + "이고 볼륨은" + volume + "입니다.");
+		System.out.println("채널은 " + channel + "이고 볼륨은 " + volume + "입니다.");
 	}
 
-	//텔레비전의 현재 채널을 반환하는 메소드 추가
+	//현재 채널은 반환하는 메소드 추가
 	int getChannel(){
 		return channel;
 	}
 
+	//텔레비전의 채널을 메소드를 통해 설정
+	void setChannel(int ch){
+		channel = ch;
+	}
+
 	public static void main(String[] args) {
 		
-		//객체 myTV 생성
+		//텔레비전 객체 myTV 생성
 		Television myTV = new Television();
 		
 		myTV.channel = 9; //myTV의 채널을 9로 설정 
@@ -25,16 +30,18 @@ public class Television {
 		myTV.getChannel(); //myTV의 채널 반환
 		myTV.print(); //텔레비전 객체 상태 출력
 		
-		//객체 yourTV 생성
+		//텔레비전 객체 yourTV 생성
 		Television yourTV = new Television();
 		
-		yourTV.channel = 9; //yourTV의 채널을 9로 설정
+		yourTV.setChannel(9); //yourTV의 채널을 9로 설정
 		yourTV.volume = 12; //yourTV의 볼륨을 12로 설정
 		yourTV.isOn = false; //yourTV의 on/off 상태를 off로 설정
 		yourTV.getChannel(); //yourTV의 채널 반환
 		yourTV.print(); //텔레비전 객체 상태 출력
-		
 	}
 }
 
 	
+
+	
+
