3d2a0c2046c0e80ac083223432c06e2d91790f4f
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-03-06 20:13:24 +0900

    2nd Commit
    It's my 2nd commit to about OOP, to print the information about Television Object.

---
+++ Television.java
@@ -0,0 +1,39 @@
+
+public class Television {
+	int channel; //채널
+	int volume; //볼륨
+	boolean isOn; //전원 on/off 상태(true -> on / false -> off)
+
+	//객체에 대한 정보 출력
+	void print() {
+		System.out.println("채널은" + channel + "이고 볼륨은" + volume + "입니다.");
+	}
+
+	//현재 채널은 반환하는 메소드 추가
+	int getChannel(){
+		return channel;
+	}
+
+	public static void main(String[] args) {
+		
+		//객체 myTV 생성
+		Television myTV = new Television();
+		
+		myTV.channel = 9; //myTV의 채널을 9로 설정 
+		myTV.volume = 7; //myTV의 볼륨을 7로 설정
+		myTV.isOn = true; //myTV의 on/off 상태를 on으로 설정
+		myTV.getChannel(); //myTV의 채널 반환
+		myTV.print(); //객체 상태 출력
+		
+		//객체 yourTV 생성
+		Television yourTV = new Television();
+		
+		yourTV.channel = 9; //yourTV의 채널을 9로 설정
+		yourTV.volume = 12; //yourTV의 볼륨을 12로 설정
+		yourTV.isOn = false; //yourTV의 on/off 상태를 off로 설정
+		yourTV.getChannel(); //yourTV의 채널 반환
+		yourTV.print(); //객체 상태 출력
+	}
+}
+
+	
