d0fe28fa07c7bbca1dd2a935f2e8c51dddfa873e
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-03-14 00:00:19 +0900

    1. 입출력과 사칙연산

---
+++ Baekjoon/baekjoon10430.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class baekjoon10430 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int a, b, c;
+        a = input.nextInt();
+        b = input.nextInt();
+        c = input.nextInt();
+
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a * b) % c);
+        System.out.println(((a % c) * (b % c)) % c);
+    }
+}
