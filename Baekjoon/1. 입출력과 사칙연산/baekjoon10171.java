643ff7f6f16a737e2324c7d87f8668397a45f8a3
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-03-14 00:09:16 +0900

    Add files via upload

---
+++ Baekjoon/1. 입출력과 사칙연산/baekjoon10171.java
@@ -0,0 +1,15 @@
+public class baekjoon10171 {
+    public static void main(String[] args){
+        //고양이와 강아지 모양을 출력하는 프로그램
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
