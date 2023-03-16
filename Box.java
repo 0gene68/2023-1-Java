37c396d8bfe13cc09973be0f1f7a354b0ce4194d
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-03-16 23:41:12 +0900

    Add files via upload
    Add a constructor that initializes the width, length, and height of the box, and add a method that calculates the volume to calculate the volume of the box after object creation.

---
+++ Box.java
@@ -0,0 +1,24 @@
+package test;
+
+public class Box {
+    private int width;
+    private int length;
+    private int height;
+    private int volume;
+
+    public int getVolume(){
+        return volume;
+    }
+
+    public Box(int w, int l, int h){
+        this.width = w;
+        this.length = l;
+        this.height = h;
+        volume = w * l * h;
+    }
+
+    public static void main(String[] args){
+        Box b = new Box(20, 30, 30);
+        System.out.println("상자의 부피는 " + b.getVolume() + "입니다.");
+    }
+}
