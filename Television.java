034f7807615162641debe01abe431178d7dbb20b
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-03-16 23:38:05 +0900

    Add files via upload
    Source code that outputs the information of the Television object using the constructor of the Television object

---
+++ Television.java
@@ -0,0 +1,25 @@
+package test;
+
+public class Television {
+    private int channel;
+    private int volume;
+    private boolean onOff;
+
+    public Television(int channel, int volume, boolean onOff){
+        this.channel = channel;
+        this.volume = volume;
+        this.onOff = onOff;
+    }
+
+    void print(){
+        System.out.println("채널은" + channel + "이고 볼륨은 " + volume + "입니다.");
+    }
+
+    public static void main(String[] args){
+        Television tv1 = new Television(7, 9, true);
+        Television tv2 = new Television(9, 12, false);
+        tv1.print();
+        tv2.print();    
+    }
+}
+
