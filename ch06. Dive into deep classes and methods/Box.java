5cec3f9e30d2fd77afdad4df007342470b9f61e6
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-03-18 01:17:56 +0900

    23. 03. 17 LAB

--- ch06. Dive into deep classes and methods/Box.java
+++ ch06. Dive into deep classes and methods/Box.java
@@ -1,25 +1,32 @@
-package test;
+package test2;
 
 public class Box {
-    private int width;
-    private int length;
-    private int height;
-    private int volume;
-
-    public int getVolume(){
-        return volume;
-    }
-
+    private int width, length, height;
+    
     public Box(int w, int l, int h){
         this.width = w;
         this.length = l;
         this.height = h;
-        volume = w * l * h;
+    }
+    
+    public int getVolume(){
+        return width * length * height;
+    }
+
+    public static Box whatisLargerBox(Box box1, Box box2){
+        if(box1.getVolume() > box2.getVolume()){
+            return box1;
+        }
+        return box2;
     }
 
     public static void main(String[] args){
-        Box b = new Box(20, 30, 30);
-        System.out.println("상자의 부피는 " + b.getVolume() + "입니다.");
-    }
-}
+        Box b1 = new Box(10, 20, 50);
+        Box b2 = new Box(10, 30, 30);
+        System.out.println("첫 번째 박스의 부피: " + b1.getVolume());
+        System.out.println("두 번째 박스의 부피: " + b2.getVolume());
 
+        Box largerBox = Box.whatisLargerBox(b1, b2);
+        System.out.println("더 큰 박스의 부피: " + largerBox.getVolume());
+    }   
+}
