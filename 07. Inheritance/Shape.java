6b69636cf3bc813fb2ac50d87e5d67577785d6bf
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-03-23 14:49:46 +0900

    23. 03. 23 LAB

---
+++ 07. Inheritance/Shape.java
@@ -0,0 +1,21 @@
+package ch07;
+
+public class Shape {
+
+    /* 
+     * The Shape class represents a general two-dimensional shape.
+     * - int x and int y are the top left coordinates of the shape.
+     * - In print(), the position of the figure is printed on the screen.
+     * 
+     * The Rectangle class is a subclass of the Shape class, and adds a number of methods along with the length and width of the rectangle.
+     * - int width and int height are the width and height of the rectangle.
+     * - In draw(), the position and size of the rectangle are displayed on the screen.
+     * - Area() calculates and returns the area of a rectangle.
+    */
+
+    int x, y;
+
+    public void print(){
+        System.out.println("x좌표 : " + x + " y좌표 : " + y);
+    }
+}
