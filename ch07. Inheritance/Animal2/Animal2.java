7dac18e67f8164d158ccee601d981f27214f0a1b
0gene68 <126875714+0gene68@users.noreply.github.com>
2023-03-31 17:21:33 +0900

    23. 03. 31 LAB

---
+++ ch07. Inheritance/Animal2/Animal2.java
@@ -0,0 +1,13 @@
+package ch07.Animal2;
+
+public class Animal2{
+
+    /*
+     * Create classes representing dogs and cats, and define the Animal class as the parent class of these classes.
+     * Write a program so that each animal's sound is output when the sound() method of the Dog and Cat classes is called.
+     */
+
+    void sound(){
+        System.out.println("Animal class' sound()");
+    }
+}
